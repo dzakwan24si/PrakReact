87175764dd4c898d63ba86b63dd1f24323ad19cb
dzakwan24si <dzakwan24si@mahasiswa.pcr.ac.id>
2026-05-26 15:30:21 +0700

    Menggunakan ShadCN UI

--- src/pages/FiturXyz.jsx
+++ src/pages/FiturXyz.jsx
@@ -1,17 +1,48 @@
 import { useState } from "react";
 import PageHeader from "../components/PageHeader";
+import { Button } from "@/components/ui/button";
+import {
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Card from "@/components/DataDisplayComponents/Card";
+import Badge from "@/components/BasicComponents/Badge";
 
 export default function FiturXyz() {
-    return (
-        <div className="p-4 animate-fade-in">
-            <PageHeader title="Fitur XYZ" breadcrumb={["Fitur XYZ", "List"]}>
-                <button 
-                    onClick={() => setIsModalOpen(true)} 
-                    className="bg-hijau text-white px-6 py-2 rounded-lg font-bold shadow-md hover:bg-[#009e68] transition transform hover:-translate-y-0.5"
-                >
-                    + Add Fitur XYZ
-                </button>
-            </PageHeader>
-        </div>
-    );
+  return (
+    <div className="p-4 animate-fade-in">
+      <PageHeader title="Fitur XYZ" breadcrumb={["Fitur XYZ", "List"]} />
+      <p>Ini Adalah Halaman Fitur XYZ</p>
+
+      <Button variant="outline">Batal</Button>
+      <Button variant="ghost">Batal</Button>
+      <Button variant="destructive">Batal</Button>
+      <Card className="mt-4 w-[380px]">
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle>Belajar shadcn/ui</CardTitle>
+            <Badge variant="secondary">Baru</Badge>
+          </div>
+          <CardDescription>
+            Contoh penggunaan komponen shadcn/ui di React
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <p className="text-sm text-muted-foreground">
+            Komponen ini dibuat di branch <strong>setup-shadcn</strong>
+            lalu di-merge ke main.
+          </p>
+        </CardContent>
+
+        <CardFooter className="flex gap-2">
+          <Button>Simpan</Button>
+          <Button variant="outline">Batal</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
